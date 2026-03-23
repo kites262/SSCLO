2d62807a9fb0b871a59cd8912b499e281abef944
kites262 <47177568+kites262@users.noreply.github.com>
2026-03-23 19:56:38 +0800

    feat: v4

--- playground/dataset_shape.py
+++
@@ -1,9 +0,0 @@
-import utils.data_load_operate as data_load_operate
-
-data_set_name_list = ['HanChuan', 'HongHu']
-data_set_path = './data/'
-
-if __name__ == '__main__':
-    for name in data_set_name_list:
-        data, gt = data_load_operate.load_data(name, data_set_path)
-        print(f"Dataset: {name}, Data shape: {data.shape}, GT shape: {gt.shape}")
--- playground/swan.py
+++
@@ -1,9 +0,0 @@
-import importlib.metadata as m
-
-import swanlab
-
-print(swanlab.__version__)
-
-
-for r in m.requires("mamba-ssm"):
-    print(r)
